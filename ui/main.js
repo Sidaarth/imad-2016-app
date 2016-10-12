9f5eb0d7268a1065bf459860210b4261e8d23cc1
Sidaarth <sidaarth.r@gmail.com>
2016-10-12 11:28:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,4 +3,10 @@ console.log('bing');
 
 var element =  document.getElementById('main-text');
 
-element.innerHTML = 'New Value'
+element.innerHTML = 'New Value';
+
+var img = document.getElementById('img');
+img.onclick = function()
+{
+  img.style.marginleft = '100px';
+};
