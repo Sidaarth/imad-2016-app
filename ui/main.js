5257aace00a10f28313ee83c3743872dba2c742c
Sidaarth <sidaarth.r@gmail.com>
2016-10-12 14:40:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,26 @@
 // counter code
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function() {
   
   
+  // create a request object
+  var request = new XMLHttpRequest();
   
   
-  
-  
-  counter = counter +1;
+// Capture the respone and store it in a variable.
+request.onreadystateexchange = function() {
+    if(request.readyState == XMLHttpRequest.DONE){
+        //take some action.
+        if(request.statuse == 200){
+            var counter = request.responseText
   var span = document.getElementById('count');
   span.innerHTML = counter.toString();
+        }
+    }
+    // Not Done yet.
+};
+//make a request
+  request.open('GET', 'http://sidaarth.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
