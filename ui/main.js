70167a537db5269bb08aa4d2231cd7be29f32d10
Sidaarth <sidaarth.r@gmail.com>
2016-10-12 22:12:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,3 +24,18 @@ request.onreadystatechange = function() {
   request.open('GET', 'http://sidaarth.imad.hasura-app.io/counter',true);
   request.send(null);
 };
+// submit name
+var nameInput = document.getElementbyId('name');
+var name = nameInput.value;
+var submit = documentElementById('submit_bth');
+submit.onClick = function(){
+  // Make request to the server and send the name.
+  // capture a list of names and render it as a list
+  var names = ['name1', 'name2', 'name3'];
+  var list = '';
+  for(var i=0; i< names.length; i++){
+      list += '<li>' + names[i] + </li>;
+  }
+  var ul = document.getElementById('namelist');
+  ui.innerHTML = list;
+};
