ef045d3efaeddfb6455607074f3f55e20c4b544c
Sidaarth <sidaarth.r@gmail.com>
2016-10-12 12:08:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,14 @@
-console.log('loaded!');
-// change the text of the main text
-
-var element =  document.getElementById('main-text');
-
-element.innerHTML = 'New Value';
-var img = document.getElementById('madi');
-var marginleft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
-}
-
-img.onclick = function()
-{
-  var interval = setInterval(moveRight, 100);
+// counter code
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function() {
+  
+  
+  
+  
+  
+  
+  counter = counter +1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
 };
