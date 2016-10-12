c636ff0533cea39241a7486e7a6c707cfd4b7068
Sidaarth <sidaarth.r@gmail.com>
2016-10-12 22:53:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,41 +1,51 @@
 // counter code
 var button = document.getElementById('counter');
 
-button.onclick = function() {
-  
-  
-  // create a request object
+var button = document.getElementById('counter');
+button.onclick = function(){
   var request = new XMLHttpRequest();
   
-  
-// Capture the respone and store it in a variable.
-request.onreadystatechange = function() {
-    if(request.readyState === XMLHttpRequest.DONE) {
-        //take some action.
-        if(request.status === 200){
-            var counter = request.responseText;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
-        }
-    }
-    // Not Done yet.
-};
-//make a request
-  request.open('GET', 'http://sidaarth.imad.hasura-app.io/counter',true);
-  request.send(null);
-};
-// submit name
-var nameInput = document.getElementbyId('name');
-var name = nameInput.value;
-var submit = documentElementById('submit_bth');
-submit.onClick = function(){
-  // Make request to the server and send the name.
-  // capture a list of names and render it as a list
-  var names = ['name1', 'name2', 'name3'];
-  var list = '';
-  for(var i=0; i< names.length; i++){
-      list += '<li>' + names[i] + '</li>';
+  request.onreadystatechange = function(){
+  if(request.readyState === XMLHttpRequest.DONE)
+  {
+      if(request.status === 200)
+      {
+          var counter = request.responseText;
+          var span = document.getElementById('count');
+          span.innerHTML = counter.toString();
+          
+      }
   }
-  var ul = document.getElementById('namelist');
-  ui.innerHTML = list;
+  };
+  
+  request.open('GET','http://sidaarth.imad.hasura-app.io/',true);
+  request.send(null);
+  
+
 };
+
+var submit = document.getElementById('Submit_btn');
+submit.onclick = function () {
+     var request = new XMLHttpRequest();
+  
+  request.onreadystatechange = function(){
+  if(request.readyState === XMLHttpRequest.DONE)
+  {
+      if(request.status === 200)
+      {
+           var names = request.responseText;
+           names = JSON.parse(names);
+           var list ='';
+            for(var i=0 ; i< names.length ; i++){
+                 list+= '<li>'+ names[i]+ '</li>' ;
+                }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
+      }
+  }
+  };
+  var nameInput = document.getElementById('name');
+var name = nameInput.value;
+  request.open('GET','http://sidaarth.imad.hasura-app.io/'+ name,true);
+  request.send(null);
+  };
